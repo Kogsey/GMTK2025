9fe816b03a53287b6b25d9f46f2dd606103fc950
MelBolt <68885800+MelBolt@users.noreply.github.com>
2025-08-02 23:54:29 +0800

    Link home to game levels

--- Assets/Scripts/Helpers/StartGameCollider.cs
+++ Assets/Scripts/Helpers/StartGameCollider.cs
@@ -1,12 +1,13 @@
 // Ignore Spelling: Collider
 
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class WinGameCollider : MonoBehaviour
+public class StartGameCollider : MonoBehaviour
 {
 	private void OnTriggerEnter2D(Collider2D collision)
 	{
 		if (collision.TryGetComponent(out PlayerController _))
-			StateManager.WinGame();
+			SceneManager.LoadScene(StateManager.GameScreen);
 	}
 }
